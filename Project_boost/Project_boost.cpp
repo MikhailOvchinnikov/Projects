c523284d9fd8df29294edd59626597eb190eae3a
MikhailOvchinnikov <ovchinnikov.ma@phystech.edu>
2021-02-10 19:25:55 +0300

    fifth change

--- Project_boost/Project_boost.cpp
+++ Project_boost/Project_boost.cpp
@@ -11,7 +11,7 @@ int main()
     std::cout << "Today, I talk to you about...";
     std::cout << "Goodby";
     std::cout << "The end";
-    std::cout << "The end ¹3";
+    std::cout << "The end ¹3(second attempt";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
