077cc12daa22037ef15dc24886c2d4550bee30a2
MikhailOvchinnikov <ovchinnikov.ma@phystech.edu>
2021-02-10 18:21:29 +0300

    Add age

--- Project_boost/Project_boost.cpp
+++ Project_boost/Project_boost.cpp
@@ -6,7 +6,7 @@
 
 int main()
 {
-    std::cout << "Hello World, I'm Mikhail!\n";
+    std::cout << "Hello World, I'm Mikhail, I'm nineteen!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
